f5cd4510eb98e2e93129ca81e03094d821c29614
sbx_user1051 <sbx_user1051@169.254.60.5>
2019-07-10 13:46:10 +0000

    Wed Jul 10 13:46:10 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,17 @@
 
 
+RULES = {
+    "A": 0,
+    "B": 0,
+    "C": 0,
+    "D": 0,
+}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    basket = {}
+    for i in skus:
+        if 
+
     raise NotImplementedError()
+
