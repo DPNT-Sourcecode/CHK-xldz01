25e4af621be5d0d3210ec084534e4641a449bbbe
sbx_user1051 <sbx_user1051@169.254.161.221>
2019-07-10 14:27:26 +0000

    Wed Jul 10 14:27:26 UTC 2019

--- test/solution_tests/CHK_R1/test_price.py
+++ test/solution_tests/CHK_R1/test_price.py
@@ -4,5 +4,8 @@ from lib.solutions.CHK import checkout_solution
 class TestSum:
     def test_checkout(self):
         assert checkout_solution.checkout("AAA") == 130
-        assert checkout_solution.checkout("BB") == 130
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("") == 0
+        assert checkout_solution.checkout("a") == -1
+
 
