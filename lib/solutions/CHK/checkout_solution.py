38a5863a59b4490257aa99273e25671703cf1565
sbx_user1051 <sbx_user1051@169.254.60.5>
2019-07-10 13:49:10 +0000

    Wed Jul 10 13:49:10 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,11 @@
+class Discount:
+    def __init__(self, price, quantity):
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_for(self, quantity):
+        return quantity * self.price / self.quantity
+
 
 
 RULES = {
@@ -10,8 +18,16 @@ RULES = {
 # skus = unicode string
 def checkout(skus):
     basket = {}
+    # scan the basket
+    # return -1 if found invalid item
     for i in skus:
-        if 
+        if i not in RULES:
+            return -1
+        if i not in basket:
+            basket[i] += 1
+        else:
+            basket[i] = 1
 
     raise NotImplementedError()
+
 
